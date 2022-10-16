af893bbc35d9663d5f9c6608ce6415713b8b0883
cristian-iordache <iordachecristian000@gmail.com>
2022-10-16 16:50:59 +0300

    update

---
+++ helper.py
@@ -0,0 +1,5 @@
+# When to use class methods and when to use static methods?
+
+class item:
+    @staticmethod
+    def is_integer():
--- main.py
+++ main.py
@@ -72,4 +72,13 @@ class Item:
 # Item.instantiate_from_csv()
 # print(Item.all)
 
-print(Item.is_integer(7.0))
+# print(Item.is_integer(7.0))
+
+class Phone(Item):
+    pass
+
+
+phone1 = Phone("jscPhonev10", 500, 5)
+phone1.broken_phones = 1
+phone2 = Phone("jscPhonev20", 700, 5)
+phone2.broken_phones = 1
